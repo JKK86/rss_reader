7071fdbd964a605c59142f4f47d8d1b85a960fa6
JKK86 <j.kurowski86@wp.pl>
2021-11-27 17:10:41 +0100

    Create a django custom command for fetching and saving the new content

---
+++ reader_app/management/commands/start_jobs.py
@@ -0,0 +1,32 @@
+import feedparser
+from dateutil import parser
+from django.core.management import BaseCommand
+
+from reader_app.models import Content, Channel, Category
+
+
+def save_new_content(channel, feed):
+    for item in feed.entries:
+        if not Content.objects.filter(guid=item.guid).exists():
+            Content.objects.create(
+                title=item.title,
+                description=item.description,
+                publication_date=parser.parse(item.published),
+                link=item.link,
+                channel=channel,
+                guid=item.guid
+            )
+
+
+def fetch_content(channel):
+    feed = feedparser.parse(channel.link)
+    save_new_content(channel, feed)
+
+
+class Command(BaseCommand):
+    help = 'Get new feeds'
+
+    def handle(self, *args, **options):
+        channels = Channel.objects.all()
+        for channel in channels:
+            fetch_content(channel)
