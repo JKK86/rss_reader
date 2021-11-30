68a4862de060d494027666ed8998530adfa0bf12
JKK86 <j.kurowski86@wp.pl>
2021-11-30 20:40:19 +0100

    Add channel list and channel detail views and templates

--- reader_app/urls.py
+++ reader_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from reader_app.views import LandingPageView
+from reader_app.views import ChannelListView, ChannelDetailView
 
 urlpatterns = [
-    path('', LandingPageView.as_view(), name='landing_page')
+    path('', ChannelListView.as_view(), name='channel_list'),
+    path('category/<slug:category_slug>/', ChannelListView.as_view(), name='channel_list_by_category'),
+    path('<slug:slug>/', ChannelDetailView.as_view(), name='channel_detail'),
 ]
--- reader_app/views.py
+++ reader_app/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateResponseMixin
 
-from reader_app.models import Content
+from reader_app.models import Content, Channel, Category
 
 
-class LandingPageView(ListView):
-    template_name = "rss_reader/landing_page.html"
-    model = Content
+class ChannelListView(TemplateResponseMixin, View):
+    model = Channel
+    template_name = "rss_reader/channel_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(LandingPageView, self).get_context_data(**kwargs)
-        context["contents"] = Content.objects.filter()[:10]
-        return context
+    def get(self, request, category_slug=None):
+        channel_list = Channel.objects.all()
+        categories = Category.objects.annotate(total_channels=Count('channels'))
+        category = None
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            channel_list = channel_list.filter(category=category)
+        paginator = Paginator(channel_list, 10)
+        page = request.GET.get('page', 1)
+        try:
+            channels = paginator.page(page)
+        except PageNotAnInteger:
+            channels = paginator.page(1)
+        except EmptyPage:
+            channels = paginator.page(paginator.num_pages)
+
+        return self.render_to_response({'categories': categories,
+                                        'category': category,
+                                        'channels': channels})
+
+
+class ChannelDetailView(DetailView):
+    model = Channel
+    template_name = "rss_reader/channel_detail.html"
