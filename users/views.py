2d4aba76acda3367684cdb6d33790fdf547d7930
JKK86 <j.kurowski86@wp.pl>
2021-12-01 15:41:42 +0100

    Add custom user creation form for registration

--- users/views.py
+++ users/views.py
@@ -1,18 +1,19 @@
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from users.forms import CustomUserCreationForm
+
 
 class RegistrationView(CreateView):
     template_name = 'registration/register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('user_content_list')
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('channel_list')
 
     def form_valid(self, form):
         result = super(RegistrationView, self).form_valid(form)
         cd = form.cleaned_data
-        user = authenticate(username=cd['username'], password=cd['password'])
+        user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
